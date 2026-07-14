5297ccc5672b303a482184cdc99d6dc9e77b6b26
Maksim <malutinm11@gmail.com>
2026-07-15 01:41:45 +0700

    created: структура проекта

---
+++ js/index.js
@@ -0,0 +1,22 @@
+const DOMContentLoadedListener = (e) => {
+
+
+    const initSwiperModule = () => {
+
+    }
+
+    const changeCurrencyModule = () => {
+
+    }
+
+    const showCatalogeModule = () => {
+
+    }
+
+
+    initSwiperModule();
+    changeCurrencyModule();
+    showCatalogeModule();
+}
+
+document.addEventListener('DOMContentLoaded', DOMContentLoadedListener);
